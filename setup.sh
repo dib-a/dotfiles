cb087eadf9a270ab791f2305c1af29fab4f65db1
dib-a <dib.ahmad.000@gmail.com>
2023-07-02 08:46:39 +0200

    added xinitrc

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Remove available configs
 rm /home/$USER/.bashrc
 rm /home/$USER/.bash_profile
+rm /home/$USER/.xinitrc
 rm /home/$USER/.config/kitty
 rm /home/$USER/.config/nvim
 rm /home/$USER/.config/picom
@@ -11,6 +12,7 @@ rm /home/$USER/.config/ranger
 # Copy configs to directory
 cp bash_profile /home/$USER/.bash_profile
 cp bashrc /home/$USER/.bashrc
+cp xinitrc /home/$USER/.xinitrc
 mkdir /home/$USER/.config
 cp -r kitty /home/$USER/.config/
 cp -r nvim /home/$USER/.config/
