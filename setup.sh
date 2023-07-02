876478027c064c236686750a00a3f41a2daf55e2
dib-a <dib.ahmad.000@gmail.com>
2023-07-02 07:04:26 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Remove available configs
+rm /home/$USER/.bashrc
+rm /home/$USER/.bash_profile
+rm /home/$USER/.config/kitty
+rm /home/$USER/.config/nvim
+rm /home/$USER/.config/picom
+rm /home/$USER/.config/ranger
+
+# Copy configs to directory
+cp bash_profile /home/$USER/.bash_profile
+cp bashrc /home/$USER/.bashrc
+cp -r kitty /home/$USER/.config/
+cp -r nvim /home/$USER/.config/
+cp -r picom /home/$USER/.config/
+cp -r ranger /home/$USER/.config/
